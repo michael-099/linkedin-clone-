ecb0bf70cdfdaf2fe9820cb5e4a4850adf80f3a1
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-06-27 14:52:10 +0300

    search

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,79 @@
 import styled from "styled-components";
-const Header = (probs)=>{
-    return(<div>Header</div>)
+const Header = (probs) => {
+    return (
+        <Container>
+            <Content>
+                <Logo><a href="/home"><img src="/img/in.svg" /></a></Logo>
+                <Search>
+                    <div><input type="text" placeholder="Search" /></div>
+                    <Sicon>
+                        <img src="img/search.svg" />
+                    </Sicon>
+                </Search>
+
+            </Content>
+        </Container>
+    )
 }
+const Container = styled.div`
+background-color:white;
+border-bottom:1px solid rgba(0,0,0,0.08);
+left:0;
+padding:0 24px;
+position:fixed;
+top:0;
+width:100vw;
+z-index:100;
+
+`;
+const Content = styled.div`
+display:flex;
+align-items:center;
+margin:0 auto;
+min-height:100%;
+${'' /* max-width:1128px; */}
+`;
+const Logo = styled.span`
+margin-right:8px;
+font-size:0px;
+
+`;
+const Search = styled.div`
+opacity:1;
+flex-grow:1;
+poaition:relative;
+&>div{ 
+    max-width:280px;
+    input{
+        border:none;
+        box-shadow:none;
+        background-color:#eef3f8;
+        border-radius:2px;
+        color:rgba(0,0,0,0.9);
+        width:218px;
+        padding:0 8px 0px 40px;
+        line-height:1.75px;
+        font-weight:400;
+        font-size:14px;
+        height:34px;
+        border-color:#dce6f1;
+        vertical-align:text-top;
+
+}}
+`;
+const Sicon = styled.div`
+width:40px;
+position:absolute;
+z-index:1;
+top:10px;
+left:80px;
+border-radius:0 2px 2px 0;
+margin:0;
+pointer-events:none;
+display:flex;
+justify-content:center;
+align-items:center;
+
+
+`;
 export default Header;
