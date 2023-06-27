b336552c5a45191e7233d2e7d7853d72fdbe7f36
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-06-27 12:28:30 +0300

    some routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import './App.css';
 import Login from './components/login';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './components/Home';
+import Header from './components/Header';
 
 
 function App() {
@@ -9,6 +11,10 @@ function App() {
     <div className="App"><Router>
       <Switch>
         <Route exact path="/"><Login /></Route>
+        <Route path="/home">
+          <Header />
+          <Home />
+        </Route>
       </Switch>
     </Router>
     </div>
