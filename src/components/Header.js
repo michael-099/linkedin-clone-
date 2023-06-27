050beb770558b35c7b4151442e75ad54f41c0110
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-06-27 12:22:47 +0300

    new files

---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+const Header = (probs)=>{
+    return(<div>Header</div>)
+}
+export default Header;
---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+const Home = (probs) => {
+    return (
+        <div>
+            Home
+        </div>)
+}
+export default Home;
