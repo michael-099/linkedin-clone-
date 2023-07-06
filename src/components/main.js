d1a4b3e9d011faa985cf142d9a71c514672125ae
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-07-06 15:23:13 +0300

    components

--- src/components/main.js
+++ src/components/main.js
@@ -2,12 +2,31 @@ import styled from "styled-components";
 const Main=(probs)=>{
     return(
     <Container>
-    main
+    <ShareBox>Share</ShareBox>
 
     </Container>)
 }
 const Container=styled.div`
 grid-area:"Main";
+`;
+const CommomCard=styled.div`
+text-align:center;
+overflow:hidden;
+margin-bottom:8px;
+background-color:#fff;
+border-radius:5px;
+position:relative;
+border:none;
+box_shadow: 0 0 0 1px rgb(0 0 0/15%), 0 0 0 rgb(0 0 0 /20%);
+`;
+const ShareBox=styled(CommomCard)`
+display:flex;
+flex-direction:column;
+color:#958b7b;
+margin:0 0 8px;
+background:white;
+
+
 `;
 
 export default Main ;
